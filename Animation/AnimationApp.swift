66509f798c0f6e2c9ce674025e1f4e8030a3e697
Peter Molnar <petermolnar.hu@gmail.com>
2021-10-27 15:42:39 +0200

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Peter Molnar on 27/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
